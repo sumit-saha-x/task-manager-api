58516f0430782a809ac702595a787457e7d07945
Inferno-SS5 <sumitsaha.ps@gmail.com>
2025-06-27 23:22:07 +0530

    CICD set

--- app.js
+++ app.js
@@ -2,6 +2,21 @@ const express = require('express');
 const sequelize = require('./db/db');
 const Task = require('./models/task');
 const taskRoutes = require('./routes/taskRoutes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+
+const swaggerSpec = swaggerJsdoc({
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Task Manager API',
+      version: '1.0.0',
+    },
+  },
+  apis: ['./routes/*.js'], // adjust to your route file
+});
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 const app = express();
 app.use(express.json());
